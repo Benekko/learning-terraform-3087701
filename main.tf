a4d3a2964b7b16256239f517bde4ca5abeb3bc91
Benekko <165157245+Benekko@users.noreply.github.com>
2024-05-10 11:35:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,6 +53,8 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
